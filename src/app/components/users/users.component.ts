4324abf269e205232b3001276bd036a0fe09d68e
Sveta-Kryukova <svatlana.kryukova.job@gmail.com>
2023-08-18 16:12:00 +0300

    add styles to users table and sorting by name

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,17 +9,28 @@ import { Router } from '@angular/router';
 })
 export class UsersComponent {
   users: User[] = [
-    {id: 1, name: 'User 1', permissions: ['read'], date: '2023-08-17'},
-    {id: 2, name: 'User 2', permissions: ['read', 'write'], date: '2023-08-17'},
-    {id: 3, name: 'User 3', permissions: ['read', 'write', 'delete'], date: '2023-08-17'},
-    {id: 4, name: 'User 4', permissions: ['read', 'write', 'delete', 'create'], date: '2023-08-17'},
-    {id: 5, name: 'User 5', permissions: ['read', 'write', 'delete', 'create', 'update'], date: '2023-08-17'}
+    { id: 1, name: 'User 1', permissions: ['read'], date: '2023-08-17' },
+    { id: 2, name: 'User 2', permissions: ['read', 'write'], date: '2023-08-17' },
+    { id: 3, name: 'User 3', permissions: ['read', 'write', 'delete'], date: '2023-08-17' },
+    { id: 4, name: 'User 4', permissions: ['read', 'write', 'delete', 'create'], date: '2023-08-17' },
+    { id: 5, name: 'User 5', permissions: ['read', 'write', 'delete', 'create', 'update'], date: '2023-08-17' }
   ];
-  constructor(private router: Router) { 
-  }
+  currentSortColumn: string = '';
+  isSortAscending: boolean = true;
+
+  constructor(private router: Router) { }
 
   sort(column: string) {
-    console.log(column);
+    if (this.currentSortColumn === column) {
+      this.isSortAscending = !this.isSortAscending;
+    } else {
+      this.currentSortColumn = column;
+      this.isSortAscending = true;
+    }
+  
+    this.users.sort((a, b) => {
+      return this.isSortAscending ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+    });
   }
 
   naviateToUser(id: number) {
