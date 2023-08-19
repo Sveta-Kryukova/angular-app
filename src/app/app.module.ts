583ede0de7aa5f0fe0608da186d1320e8103b6b9
Sveta-Kryukova <svatlana.kryukova.job@gmail.com>
2023-08-19 13:01:27 +0300

    simple log out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { UsersComponent } from './components/users/users.component';
 import { SingleUserComponent } from './components/single-user/single-user.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { CountriesComponent } from './components/countries/countries.component';
-
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { CountriesComponent } from './components/countries/countries.component';
     LoginComponent,
     UsersComponent,
     SingleUserComponent,
-    CountriesComponent
+    CountriesComponent,
   ],
   imports: [
     BrowserModule,
